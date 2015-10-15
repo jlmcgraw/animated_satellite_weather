05dced348eb29ed4f27045a707177143620bb24b
Jesse McGraw <jlmcgraw@gmail.com>
2015-10-15 15:09:43 -0400

    More imagery script to update imagery and movies repeatedly

---
+++ continuous_update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -eu                # Always put this in Bourne shell scripts
+IFS=$(printf '\n\t')  # Always put this in Bourne shell scripts
+
+#Update the imagery and recreate the movies every 6 hours
+
+while true 
+    do
+        ./freshenLocalData.sh
+        ./makeSatelliteMovies.sh
+        sleep 6h
+    done
