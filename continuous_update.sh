585f65613e25b4c3d39168edb6eceb6bb23fa8aa
Jesse McGraw <jlmcgraw@gmail.com>
2015-10-16 09:29:51 -0400

    Minor changes

--- continuous_update.sh
+++ continuous_update.sh
@@ -3,11 +3,12 @@ set -eu                # Always put this in Bourne shell scripts
 IFS=$(printf '\n\t')  # Always put this in Bourne shell scripts
 
 #Update the imagery and recreate the movies every 6 hours
+sleep_length="1h"
 
 while true 
     do
         ./freshenLocalData.sh
         ./makeSatelliteMovies.sh
-        echo "Sleeping..."
-        sleep 6h
+        echo "Sleeping for $sleep_length at $(date)"
+        sleep $sleep_length
     done
